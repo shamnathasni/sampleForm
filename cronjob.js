951f794bd5c1830c6f05f3e584a63573a18d9a8f
shamnathasni <shamnathasni4@gmail.com>
2024-12-23 21:31:32 +0530

    db modified

--- cronjob.js
+++ cronjob.js
@@ -1,9 +1,13 @@
-const maxRetries = 6;
-const pingServer = (retries = 0) => {
+import cron from "node-cron";
+import https from "https";
+
+const job = cron.schedule("*/14 * * * *", () => {
+  console.log("Pinging server to keep it alive...");
+
   const options = {
     hostname: "swoz.onrender.com",
     method: "GET",
-    timeout: 120000, // Timeout set to 120 seconds
+    timeout: 60000, // Timeout set to 60 seconds
   };
 
   const req = https.request(options, (res) => {
@@ -13,31 +17,13 @@ const pingServer = (retries = 0) => {
   req.on("timeout", () => {
     req.abort();
     console.error("Request timed out");
-
-    if (retries < maxRetries) {
-      console.log(`Retrying... Attempt ${retries + 1}`);
-      setTimeout(() => pingServer(retries + 1), 1000 * (retries + 1)); // Exponential backoff
-    } else {
-      console.log("Max retries reached. No further attempts.");
-    }
   });
 
   req.on("error", (err) => {
     console.error("Ping error:", err.message);
-
-    if (retries < maxRetries) {
-      console.log(`Retrying... Attempt ${retries + 1}`);
-      setTimeout(() => pingServer(retries + 1), 1000 * (retries + 1)); // Exponential backoff
-    } else {
-      console.log("Max retries reached. No further attempts.");
-    }
   });
 
   req.end();
-};
+});
 
-// Pinging every 5 minutes to ensure the server stays alive
-setInterval(() => {
-  console.log("Pinging server to keep it alive...");
-  pingServer();
-}, 5 * 60 * 1000); // Every 5 minutes
+export default job;
