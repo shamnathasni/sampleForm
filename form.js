1343c47642679924b020526016d199eb36148779
shamnathasni <shamnathasni4@gmail.com>
2024-12-23 21:16:24 +0530

    db modified

--- form.js
+++ form.js
@@ -83,7 +83,7 @@ app.post("/receive", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
