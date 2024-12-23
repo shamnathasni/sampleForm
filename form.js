81c7a2cba3cfed5c963869f12244cb1204ae744a
shamnathasni <shamnathasni4@gmail.com>
2024-12-23 22:55:07 +0530

    db modified

--- form.js
+++ form.js
@@ -31,7 +31,7 @@ app.post("/submit", (req, res) => {
     .catch((error) => {
       console.log(error.message)
       console.error("Error sending data:", error);
-      res.status(500).json({ message: "Error forwarding data to /receive route" ,error:response.data});
+      res.status(500).json({ message:data.alert, ":  Error forwarding data to /receive route" });
     });
 });
 
