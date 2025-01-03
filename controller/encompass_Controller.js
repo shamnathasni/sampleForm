c2208f840975e10e3cbfb21d4d3222b6b7066521
shamnathasni <shamnathasni4@gmail.com>
2025-01-04 00:33:16 +0530

    webhook added

--- controller/encompass_Controller.js
+++ controller/encompass_Controller.js
@@ -389,19 +389,19 @@ export const getLoan =  async (req, res) => {
   // Helper Function: Create Subscription
 
       // Obtain OAuth token
-      const tokenResponse = await axios.post(
-        "https://api.elliemae.com/oauth2/v1/token",
+      const tokenResponse =  await axios.post(
+        'https://api.elliemae.com/oauth2/v1/token',
         new URLSearchParams({
-          grant_type: "password",
-          username: "chrisj@encompass:TEBE11371233",
-          password: "loAIcrm1994!",
-          client_id: "fw5t9js",
-          client_secret: "^TPShPA0#fi4jit7dJlEqBJty0w10JnRabA@SaGUq5!q",
+          grant_type: 'password',
+          username: 'chrisj@encompass:TEBE11371233',
+          password: 'loAIcrm1994!',
+          client_id: 'fw5t9js',
+          client_secret: '^TPShPA0#fi4jit7dJlEqBJl#IsK6bPCuRZONV7e1CJty0w10JnRabA@SaGUq5!q'
         }),
         {
           headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
+            'Content-Type': 'application/x-www-form-urlencoded',
+          }
         }
       );
   
