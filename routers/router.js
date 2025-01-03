e4d4e7864aed4c38bc71993ff6e07045db4b46c1
shamnathasni <shamnathasni4@gmail.com>
2025-01-04 01:09:37 +0530

    webhook added

--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import { getLoan, receiveLoan, simple, submitLoan, updateDB, } from '../controller/encompass_Controller.js'; // Correct import of the controller
+import { createSubscription, getLoan, receiveLoan, simple, submitLoan, updateDB, } from '../controller/encompass_Controller.js'; // Correct import of the controller
 
 
 // Define POST route for '/get-token' that calls the controller function
@@ -10,5 +10,19 @@ router.post('/submit', submitLoan);
 router.post('/receive', receiveLoan);
 router.get('/loan/:loanId',getLoan)
 router.post('/updateLoan',updateDB)
+
+router.patch('*', async (req, res) => {
+    try {
+      // Call createSubscription function
+      const subscriptionResponse = await createSubscription();
+      console.log('Subscription created successfully:', subscriptionResponse);
+  
+      // Send a response
+      res.status(200).json({ message: 'Subscription created successfully', subscriptionResponse });
+    } catch (error) {
+      console.error('Error creating subscription:', error.message);
+      res.status(500).json({ message: 'Failed to create subscription', error: error.message });
+    }
+  });
 
 export default router;  // Make sure to export the router
