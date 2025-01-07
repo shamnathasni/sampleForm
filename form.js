3392c577ef820239859682189c89f8dba2014679
shamnathasni <shamnathasni4@gmail.com>
2025-01-07 23:48:43 +0530

    webhook added

--- form.js
+++ form.js
@@ -18,7 +18,7 @@ app.use('/', router);
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0',() => {
   // console.log(`Server is running on port http://localhost:${PORT}`);
   console.log(`Server is running on port ${PORT}`);
 });
