9c867abd666e4d1594e0ab847584236769ac28ed
shamnathasni <shamnathasni4@gmail.com>
2025-01-08 19:43:37 +0530

    webhook added

--- form.js
+++ form.js
@@ -3,8 +3,6 @@ import axios from "axios";
 // import cronJob from "./cronjob.js";
 import mongoose from "./config/db_config.js";
 import cors from 'cors';
-import Borrower from "./Model/borrowerModel.js";
-import BorrowerProfile from "./Model/borrowerProfileModel.js";
 import router from "./routers/router.js";
 
 const app = express();
@@ -18,7 +16,9 @@ app.use('/', router);
 
 // Start the server
 const PORT = 3000;
-app.listen(PORT, () => {
-  // console.log(`Server is running on port http://localhost:${PORT}`);
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Set server timeout to 1 hour (3600000 milliseconds)
+server.setTimeout(3600000);
